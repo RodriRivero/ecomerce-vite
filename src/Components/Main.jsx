a9ce85ce7878c3f4218f8bcbb53d7d686d32c6c3
rodrigoss88 <guidorodrigorivero@outlook.com>
2023-04-05 12:15:47 -0300

    ecomerce vite

---
+++ src/Components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+*import {CiSearch,CiShoppingCart} from 'react-icons/ci'
+import { Watch } from '../assets/watch.jpg'
+import { lapto } from '../assets/laptop.jpg'
+import { keyboard } from '../assets/keyboard.jpg'
+import { sunGlass } from '../assets/glasses.jpeg'
+import { leatherWacht } from '../assets/leatherWatch.jpg'
+import { mouse } from '../assets/mouse.jpg'
+import { Monitor } from '../assets/monitor.jpg'
+
+
+const Main = () => {
+  return (
+
+  )
+}
+
+export default Main
