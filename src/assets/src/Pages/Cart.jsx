b2cc2b181ccf42964243282fc6339bf7cfc26402
rodrigoss88 <guidorodrigorivero@outlook.com>
2023-04-10 20:25:53 -0300

    ecomerce vite

--- src/assets/src/Pages/Cart.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Cartitems from '../components/Cartitems'
-export  function Cart() {
-  return (
-    <div className='ml-[80px]'>
-      <Cartitems/>
-    </div>
-  )
-}
